0b45a1efd6e2219167ac49b1771250d46a25f9ec
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-02 16:48:13 +0430

    solve the errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import QuoteBox from './QuoteBox'; // Ensure the path is correct
+import React from 'react'; // eslint-disable-line no-unused-vars
+import QuoteBox from './QuoteBox'; // eslint-disable-line no-unused-vars
 
 function App() {
-  // Use React and QuoteBox to avoid no-unused-vars errors
-  console.log(React.version, QuoteBox); // Temporary usage to avoid eslint error
-
   return (
     <div className="App">
       <QuoteBox />
